022036682f3a19ef489e33c44a9ab84f1d10104e
erickems <erick.sena@ccc.ufcg.edu.br>
2021-06-06 16:03:17 -0300

    Cadastro de usuário melhorado. Cadastro de palavra adicionado temporariamente

--- cadastro.py
+++ cadastro.py
@@ -1,10 +1,16 @@
-import os
-import json
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
 
-def cadastrarUsuario(login, senha):
+import os, json
+
+def cadastrarUsuario():
     d1 = lerUsuarios()
 
-    if exists(login, d1): return False
+    login = input('Digite o login: ')
+    senha = input('Defina uma senha: ')
+
+    if exists(login, d1): print('Usuário já cadastrado'); return False
     else: 
         d1[login] = {
             'login': login,
@@ -12,6 +18,8 @@ def cadastrarUsuario(login, senha):
             }
         with open('usuarios.json', 'w+') as file:
             json.dump(d1, file)
+            
+        print('Cadastro realizado com sucesso!')
 
 def lerUsuarios():
     usuarios = {}
@@ -22,7 +30,39 @@ def lerUsuarios():
 
     return usuarios
 
-def exists(login, dicionario):
+def cadastrarPalavra(palavra, dica):
+    d1 = lerPalavras()
+
+    if exists(palavra, d1): return False
+    else: 
+        d1[palavra] = {
+            'nome': palavra,
+            'dica': dica
+            }
+        with open('palavras.json', 'w+', encoding='utf8') as file:
+            json.dump(d1, file, ensure_ascii=False)
+
+def lerPalavras():
+    palavras = {}
+
+    if os.path.exists('palavras.json'):
+        with open('palavras.json', 'r', encoding='utf8') as file:
+            palavras = json.load(file)
+
+    return palavras
+
+def exists(chave, dicionario):
     chaves = dicionario.keys()
-    if login in chaves:
+    if chave in chaves:
         return True
+
+# def main():
+#     while(True):
+#         print(lerPalavras())
+#         palavra = input('Palavra: ')
+#         if palavra == 'exit': break
+#         dica = input('Dica: ') 
+#         cadastrarPalavra(palavra, dica)
+
+# if __name__ == '__main__':
+#     main()
