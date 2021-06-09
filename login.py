ed739b453c5b808d2b81fd15077011c37f472343
erickems <erick.sena@ccc.ufcg.edu.br>
2021-06-09 10:13:13 -0300

    Melhorias adicionadas

--- login.py
+++ login.py
@@ -1,16 +1,17 @@
 from cadastro import lerUsuarios
+import os, time
 
 def logar():
     dir_usuarios = lerUsuarios()
     chaves_usuarios = dir_usuarios.keys()
 
-    login = input('Digite seu login: ')
+    login = input('\nDigite seu login: ')
     
     if login in chaves_usuarios:
         senha = input('Digite sua senha: ')
 
-        if dir_usuarios[login]['senha'] == int(senha) or dir_usuarios[login]['senha'] == senha: print('Acesso autorizado'); return True
+        if dir_usuarios[login]['senha'] == senha or dir_usuarios[login]['senha'] == int(senha): print('Acesso autorizado!'); time.sleep(0.5); return True
         
-        else: print('Senha incorreta'); return False
+        else: print('Senha incorreta!'); time.sleep(0.7); os.system('cls' if os.name == 'nt' else 'clear'); return False
 
-    else: print('Usuario inexistente'); return False         
+    else: print('Usuario inexistente!'); time.sleep(0.7); os.system('cls' if os.name == 'nt' else 'clear'); return False         
